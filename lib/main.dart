cd0a690f6bc02c34a2f69f2d24a6b5cfd17d7139
hovadur <hovadur@gmail.com>
2020-10-10 11:55:48 +0300

    added another LoginScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
-import 'package:flutter_login/flutter_login.dart';
+import 'package:sign_button/constants.dart';
+import 'package:sign_button/create_button.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,13 +22,65 @@ class MyApp extends StatelessWidget {
 }
 
 class LoginScreen extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return FlutterLogin(loginProvidersList: [
-      LoginProvider(icon: Icons.ac_unit, callback: (){
-        return Future.delayed(Duration(milliseconds: timeDilation.ceil() * 2250)).then((value) => "null");
-      })
-    ], onSignup: null, onLogin: null, onRecoverPassword: null);
+    // https://stackoverflow.com/a/32686261/9449426
+    final email = RegExp(r'^[^\s@]+@[^\s@]+\.[^\s@]+$');
+    return Scaffold(
+      appBar: AppBar(leading: Icon(Icons.menu), title: Text("Authorization")),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: const InputDecoration(hintText: "Email"),
+                      validator: (String? value) {
+                        if (value != null && !email.hasMatch(value)) {
+                          return "Invalid email";
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(hintText: "Password"),
+                      validator: (String? value) {
+                        if (value != null && value.length < 8) {
+                          return "Must be > 7 characters";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 16),
+                        child: ElevatedButton(
+                          autofocus: true,
+                          onPressed: () {
+                            if (_formKey.currentState?.validate() == false) {
+                              return null;
+                            }
+                          },
+                          child: Text("Next"),
+                        ),
+                      ),
+                    )
+                  ],
+                )),
+            SignInButton(
+                btnText: "Sign in with Google",
+                buttonType: ButtonType.google,
+                onPressed: () {}),
+          ],
+        ),
+      ),
+    );
   }
-  
 }
