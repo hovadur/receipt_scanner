5c0983a20a5ae755805bb0561a8b3c4b7183789b
hovadur <hovadur@gmail.com>
2020-10-21 17:58:57 +0300

    DateTimePicker in ManualScreen

--- lib/presentation/manual/manual_screen.dart
+++ lib/presentation/manual/manual_screen.dart
@@ -1,4 +1,5 @@
 import 'package:ctr/l10n/app_localizations.dart';
+import 'package:ctr/presentation/common/date_time_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -11,14 +12,22 @@ class ManualScreen extends StatelessWidget {
   Widget build(BuildContext context) => ChangeNotifierProvider(
       create: (_) => ManualViewModel(),
       child: Scaffold(
-        appBar: AppBar(title: Text(AppLocalizations.of(context).manual)),
-        floatingActionButton: FloatingActionButton.extended(
-          icon: Icon(Icons.add),
-          label: Text(AppLocalizations.of(context).addProduct),
-          onPressed: () {},
-        ),
-        body: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-          InputDatePickerFormField(firstDate: DateTime.now(), lastDate: DateTime.now())
-        ]),
-      ));
+          appBar: AppBar(title: Text(AppLocalizations.of(context).manual)),
+          floatingActionButton: FloatingActionButton.extended(
+            icon: Icon(Icons.add),
+            label: Text(AppLocalizations.of(context).addProduct),
+            onPressed: () {},
+          ),
+          body: SingleChildScrollView(
+            padding: const EdgeInsets.fromLTRB(32.0, 26, 32, 32),
+            child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+              DateTimePicker(
+                locale: Localizations.localeOf(context),
+                type: DateTimePickerType.dateTimeSeparate,
+                initialValue: DateTime.now().toString(),
+                firstDate: DateTime.fromMillisecondsSinceEpoch(0),
+                lastDate: DateTime.now(),
+              )
+            ]),
+          )));
 }
