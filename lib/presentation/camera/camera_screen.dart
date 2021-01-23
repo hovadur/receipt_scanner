6015afab874a71f0dac14ab41b76b686dbb98d9e
hovadur <hovadur@gmail.com>
2021-01-23 11:22:01 +0300

    floating_action_bubble in camera_screen

--- lib/presentation/camera/camera_screen.dart
+++ lib/presentation/camera/camera_screen.dart
@@ -7,55 +7,83 @@ import 'package:ctr/presentation/drawer/main_drawer.dart';
 import 'package:ctr/presentation/fromFile/from_file_screen.dart';
 import 'package:ctr/presentation/fromParam/from_param_screen.dart';
 import 'package:ctr/presentation/manual/manual_screen.dart';
+import 'package:floating_action_bubble/floating_action_bubble.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_riverpod/all.dart';
 
 import '../../app_module.dart';
 
-class CameraScreen extends ConsumerWidget {
+class CameraScreen extends StatefulWidget {
   const CameraScreen({Key? key}) : super(key: key);
   static const String routeName = 'CameraScreen';
 
   @override
-  Widget build(BuildContext context, ScopedReader watch) => Scaffold(
-      appBar: AppBar(title: Text(context.translate().scanning)),
-      drawer: const MainDrawer(),
-      floatingActionButton:
-          Column(mainAxisAlignment: MainAxisAlignment.end, children: [
-        FloatingActionButton.extended(
-          heroTag: null,
-          icon: const Icon(Icons.add),
-          label: Text(context.translate().fromFile),
-          onPressed: () {
-            AppNavigator.of(context).push(const MaterialPage<Page>(
-                name: FromFileScreen.routeName, child: FromFileScreen()));
-          },
-        ),
-        const SizedBox(height: 8),
-        FloatingActionButton.extended(
-          heroTag: null,
-          icon: const Icon(Icons.add),
-          label: Text(context.translate().manual),
-          onPressed: () {
-            AppNavigator.of(context).push(const MaterialPage<Page>(
-                name: ManualScreen.routeName, child: ManualScreen()));
-          },
-        ),
-        const SizedBox(height: 8),
-        FloatingActionButton.extended(
-          heroTag: null,
-          icon: const Icon(Icons.add),
-          label: Text(context.translate().fromParam),
-          onPressed: () {
-            AppNavigator.of(context).push(const MaterialPage<Page>(
-                name: FromParamScreen.routeName, child: FromParamScreen()));
-          },
-        ),
-      ]),
-      body: Container(
-          constraints: const BoxConstraints.expand(),
-          child: _buildPreview(context, watch)));
+  CameraScreenState createState() => CameraScreenState();
+}
+
+class CameraScreenState extends State<CameraScreen>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> _animation;
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 260),
+    );
+
+    final curvedAnimation =
+        CurvedAnimation(curve: Curves.easeInOut, parent: _animationController);
+    _animation = Tween<double>(begin: 0, end: 1).animate(curvedAnimation);
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) => Consumer(
+      builder: (context, watch, child) => Scaffold(
+          appBar: AppBar(title: Text(context.translate().scanning)),
+          drawer: const MainDrawer(),
+          floatingActionButton: FloatingActionBubble(
+              animation: _animation,
+              onPress: () => _animationController.isCompleted
+                  ? _animationController.reverse()
+                  : _animationController.forward(),
+              iconData: Icons.add,
+              items: <Bubble>[
+                Bubble(
+                  title: context.translate().fromFile,
+                  icon: Icons.attach_file,
+                  onPress: () {
+                    AppNavigator.of(context).push(const MaterialPage<Page>(
+                        name: FromFileScreen.routeName,
+                        child: FromFileScreen()));
+                  },
+                ),
+                Bubble(
+                  title: context.translate().manual,
+                  icon: Icons.approval,
+                  iconColor: Colors.white,
+                  onPress: () {
+                    AppNavigator.of(context).push(const MaterialPage<Page>(
+                        name: ManualScreen.routeName, child: ManualScreen()));
+                  },
+                ),
+                Bubble(
+                  title: context.translate().fromParam,
+                  icon: Icons.compare_arrows,
+                  onPress: () {
+                    AppNavigator.of(context).push(const MaterialPage<Page>(
+                        name: FromParamScreen.routeName,
+                        child: FromParamScreen()));
+                  },
+                ),
+              ]),
+          body: Container(
+              constraints: const BoxConstraints.expand(),
+              child: _buildPreview(context, watch))));
 
   Widget _buildPreview(BuildContext context, ScopedReader watch) {
     final camera = watch(cameraNotifier).camera;
